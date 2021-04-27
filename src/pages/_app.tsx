a8f9ddd3bb8718bc3fc875321d15b73025bb30fd
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2021-04-27 00:34:15 -0300

    aplicação finalizada

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+import { PlayerContextProvider } from "../contexts/PlayerContexts";
+
+import styles from "../styles/app.module.scss";
+import "../styles/global.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  );
+}
+
+export default MyApp;
